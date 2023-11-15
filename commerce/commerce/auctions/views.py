b26873bc3b1bbcab7961a01cb70f246b180583a3
EJ-Lan <ervinjon_lanada@outlook.com>
2023-11-14 22:26:09 -0700

    Work on views.py Create

--- commerce/commerce/auctions/views.py
+++ commerce/commerce/auctions/views.py
@@ -5,15 +5,16 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.contrib import messages
 
-from .models import User
+from .models import Listing, User
 
 class CreateListingForm(forms.Form):
     title = forms.CharField(label='Title', widget=forms.TextInput(attrs={'class': 'form-control'}))
     content = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
     starting_bid = forms.DecimalField(max_digits=10, decimal_places=2, widget=forms.NumberInput(attrs={'class': 'form-control'}))
-    category = forms.CharField(max_length=64, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    image_url = forms.URLField(max_length=200, widget=forms.URLInput(attrs={'class': 'form-control'}))
+    category = forms.CharField(max_length=64, required=False, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    image_url = forms.URLField(max_length=200,required=False, widget=forms.URLInput(attrs={'class': 'form-control'}))
 
 
 def index(request):
@@ -70,8 +71,27 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
-    
+
+@login_required
 def create(request):
+    if request.method == "POST":
+        form = CreateListingForm(request.POST)
+        if form.is_valid():
+            new_listing = Listing(
+                owner = request.user,
+                title = form.cleaned_data['title'],
+                description = form.cleaned_data['content'],
+                start_bid = form.cleaned_data['starting_bid'],
+                category = form.cleaned_data['category'],
+                image_url = form.cleaned_data['image_url']
+            )
+
+            new_listing.save()
+            messages.success(request, 'Your listing has been created successfully!')
+            return HttpResponseRedirect(reverse("index"))
+    else:
+        form = CreateListingForm()
+
     return render(request, "auctions/create.html", {
-        "create_form": CreateListingForm()
+        "create_form": form
     })    
